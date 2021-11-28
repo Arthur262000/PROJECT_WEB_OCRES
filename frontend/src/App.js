d9918956f0d52b48e518d02557d4e906f7bfda55
Arthur262000 <arthur262000@gmail.com>
2021-11-28 01:24:55 +0100

    Menu and API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from "axios";
+import React, { useEffect } from 'react';
+import Header from './components/Header/Header';
 import './App.css';
+import { useState } from "react";
+import SingleContent from "./components/SingleContent/SingleContent";
 
 function App() {
+
+  const [content, setContent] = useState([]);
+
+  const fetchNowPlaying = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}`
+    
+    );
+
+
+    console.log(data);
+
+    setContent(data.results);
+
+  };
+
+  useEffect(() => {
+    fetchNowPlaying();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="App">
+      <span className="section">A l'Affiche</span>
+      <div className="NowPlaying">
+        {content && content.map((c) => <SingleContent 
+        key={c.id} 
+        id={c.id} 
+        poster={c.poster_path} 
+        title={c.title || c.name} 
+        date={c.first_air_date || c.release_date} 
+        vote_average={c.vote_average}
+        /> )}
+      </div>
+    </div>
     </div>
   );
-}
+};
 
 export default App;
